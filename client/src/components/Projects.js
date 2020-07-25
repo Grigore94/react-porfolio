c0d7c03053d1409b26f318df0d8c2271aa50a306
Grigore <grigore.mereacre95@gmail.com>
2020-07-25 13:13:23 -0500

    threee card to project component added and some functionality and styles

---
+++ client/src/components/Projects.js
@@ -0,0 +1,119 @@
+import React, { Component } from "react";
+
+class Projects extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: 0 };
+  }
+  toggleCategories() {
+    if (this.state.activeTab === 0) {
+      return (
+        <div className="projects-grid">
+          {/* First project here */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{ color: "#fff", height: "176px", background: "url()" }}
+            >
+              {/* Project#1 */}
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>LiveDemo</Button>
+              <Button colored>CodePen</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+
+          {/* First project here */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{ color: "#fff", height: "176px", background: "url()" }}
+            >
+              {/* Project#2 */}
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>LiveDemo</Button>
+              <Button colored>CodePen</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+          {/* First project here */}
+          <Card shadow={5} style={{ minWidth: "450", margin: "auto" }}>
+            <CardTitle
+              style={{ color: "#fff", height: "176px", background: "url()" }}
+            >
+              {/* Project#3 */}
+            </CardTitle>
+            <CardText>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's
+            </CardText>
+            <CardActions border>
+              <Button colored>Github</Button>
+              <Button colored>LiveDemo</Button>
+              <Button colored>CodePen</Button>
+            </CardActions>
+            <CardMenu style={{ color: "#fff" }}>
+              <IconButton name="share" />
+            </CardMenu>
+          </Card>
+        </div>
+      );
+    } else if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <h1>This is NodeJS</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 2) {
+      return (
+        <div>
+          <h1>This is JavaScript</h1>
+        </div>
+      );
+    } else if (this.state.activeTab === 3) {
+      return (
+        <div>
+          <h1>This is MongoDB</h1>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Tabs
+          activeTAb={this.state.activeTab}
+          onChange={(tabId) => this.setState({ activeTAb: tabId })}
+          ripple
+        >
+          <Tab>React</Tab>
+          <Tab>NodeJs</Tab>
+          <Tab>MongoDB</Tab>
+          <Tab>VanillaJS</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="content">{this.toggleCategories()}</div>
+          </Cell>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Projects;
