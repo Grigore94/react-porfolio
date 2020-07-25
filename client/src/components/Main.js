bb3d9f64483b4f3368a5ff96f641b2b9190cae86
Grigore <grigore.mereacre95@gmail.com>
2020-07-25 12:42:44 -0500

    imported components and switchto main component

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
-import LandingPage from './landingpage';
-import AboutMe from './aboutme';
-import Contact from './contact';
-import Projects from './projects';
-import Resume from './resume';
+import {LandingPage} from './landingpage';
+import {About} from './about';
+import {Contact} from './contact';
+import {Projects} from './projects';
+import {Resume} from './resume';
 
 
 const Main = () => (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/about" component={About} />
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
