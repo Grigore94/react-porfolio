5b637df760e6c47861ffed8bf008578477a10389
Grigore <grigore.mereacre95@gmail.com>
2020-07-26 11:00:23 -0500

    added content to about page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Cell } from "react-mdl";
 import Education from "./Education"
-import Eperience from "./Experience";
+// import Eperience from "./Experience";
 import SkillsLanguages from "./SkilllsLanguages";
 import Meme from "../Images/meme.jpeg"
 
@@ -19,39 +19,51 @@ class About extends Component {
               />
             </div>
             <h2 style={{ padding: "0.8em",textAlign:"center" }}>Grigore Mereacre</h2>
-            <h4 style={{ color: "gray" }}>Full Stack Web Developer</h4>
+            <h4 style={{ color: "gray" }}>FullStack Web Developer</h4>
             <hr style={{ broderTop: "3px solid #833fb2", width: "50%" }} />
-            <p>loremipsum</p>
+            <p>Hello my friend</p>
             <hr style={{ broderTop: "3px solid #833fb2", width: "50%" }} />
-            <h5>Address</h5>
-            <p>hacker way menlo park</p>
+            <h5>Address:</h5>
+            <p>Nashville Tennesse</p>
             <h5>Phone:</h5>
-            <p>6765553345</p>
+            <p>615-942-3555</p>
             <h5>Email:</h5>
-            <p>ahahdla@gmail.com</p>
+            <p>grigore.mereacre@gmail.com</p>
             <hr style={{ broderTop: "3px solid #833fb2", width: "50%" }} />
           </Cell>
           <Cell className="resume-right-col" col={8}>
-           <h2>Education</h2>
+            
+           <h2 style={{textAlign:"center"}}>Education</h2>
 
+           <Education 
+           startYear={2014}
+           endYear={2017}
+           schoolName="University of European Studies"
+           schoolDescription="Economics"
+           />
            <Education
-           startYear={2012}
-           endYear={2015}
-           schoolName="University of Cali"
-           schoolDescription="best university in the world"
+           startYear={2020}
+           endYear={2020}
+           schoolName="Nashville Software School"
+           schoolDescription=""
+           />
+           <Education
+           startYear={2020}
+           endYear={2020}
+           schoolName="Vanderbilt University Boot Camps"
+           schoolDescription="Full Stack"
            />
            <hr style={{borderTop:"3px solid #e22947"}} />
 
-           <h2>Experience</h2>
+           {/* <h2>Experience</h2>
            <Eperience
            startYear={2016}
            endYear={2017}
            jobName="Dispacher"
            jobDescription="kkdkdkdkdkd"
-           />
+           /> */}
            
-           <hr style={{borderTop:"3px solid #e22947"}} />
-           <h2>Skills</h2>
+           <h2 style={{textAlign:"center"}} >Skill Set</h2>
            <SkillsLanguages
            skill="JavaScript"
            progress={55}
@@ -74,6 +86,25 @@ class About extends Component {
            /><SkillsLanguages
            skill="MySQL"
            progress={65}
+           />
+           <hr style={{borderTop:"3px solid #e22947"}} />
+
+           <h2 style={{textAlign:"center"}} >Languages</h2>
+           <SkillsLanguages
+           skill="English"
+           progress={75}
+           />
+           <SkillsLanguages
+           skill="Romanian"
+           progress={95}
+           />
+           <SkillsLanguages
+           skill="Russian"
+           progress={85}
+           />
+           <SkillsLanguages
+           skill="Spanish"
+           progress={35}
            />
 
           </Cell>
