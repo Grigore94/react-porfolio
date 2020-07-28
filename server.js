9b6267a89d98927d0c8fd72c273f5b926886acb2
Grigore <grigore.mereacre95@gmail.com>
2020-07-28 17:23:02 -0500

    deleetd routes

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+
 
 app.post("/api/form", (req, res) => {
   console.log(req.body);
