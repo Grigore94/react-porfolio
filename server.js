7fe24c9ebd079435664896bee85fb3a6f208ac12
Grigore <grigore.mereacre95@gmail.com>
2020-07-28 12:40:14 -0500

    added conection variable

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+var connect = require("connect");
+
+var app = connect.createServer().use(connect.static(__dirname + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
